f38db3760053cb8975ab65b3ba60e905256c9588
tbui1996 <39144242+tbui1996@users.noreply.github.com>
2020-04-19 22:11:52 -0400

    Cookie Changes
    Enable url load

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function handleRequest(request) {
-    let reqBody, stringify, parsestringify, firstUrl, secondUrl;
+   let reqBody, stringify, parsestringify, firstUrl, secondUrl;
     let parsestringifylen;
 
     reqBody = 'https://cfw-takehome.developers.workers.dev/api/variants';
@@ -11,25 +11,27 @@ async function handleRequest(request) {
     firstUrl = parsestringify.variants[0];
     secondUrl = parsestringify.variants[1];
     
-    const NAME = 'experiment-0'
-    // Responses below are place holders, you could set up
-    // a custom path for each test (e.g. /control/somepath )
-    const TEST_RESPONSE =  fetch('https://cfw-takehome.developers.workers.dev/variants/1', request)
-    const CONTROL_RESPONSE =  fetch('https://cfw-takehome.developers.workers.dev/variants/2', request)
-    // Determine which group this requester is in.
-    const cookie = request.headers.get('cookie')
-    if (cookie && cookie.includes(`${NAME}=control`)) {
-      return CONTROL_RESPONSE
-    } else if (cookie && cookie.includes(`${NAME}=test`)) {
-      return TEST_RESPONSE
-    } else {
-      // if no cookie then this is a new client, decide a group and set the cookie
-      let group = Math.random() < 0.5 ? 'test' : 'control' // 50/50 split
-      let response = group === 'control' ? CONTROL_RESPONSE : TEST_RESPONSE
-      response.headers.append('Set-Cookie', `${NAME}=${group}; path=/`)
-      return response
-    }
+
+
+  const NAME = 'experiment-0'
+  // Responses below are place holders, you could set up
+  // a custom path for each test (e.g. /control/somepath )
+  const TEST_RESPONSE = new Response(firstUrl) // fetch('/test/sompath', request)
+  const CONTROL_RESPONSE = new Response(secondUrl) // fetch('/control/sompath', request)
+  // Determine which group this requester is in.
+  const cookie = request.headers.get('cookie')
+  if (cookie && cookie.includes(`${NAME}=control`)) {
+    return CONTROL_RESPONSE
+  } else if (cookie && cookie.includes(`${NAME}=test`)) {
+    return TEST_RESPONSE
+  } else {
+    // if no cookie then this is a new client, decide a group and set the cookie
+    let group = Math.random() < 0.5 ? 'test' : 'control' // 50/50 split
+    let response = group === 'control' ? CONTROL_RESPONSE : TEST_RESPONSE
+    response.headers.append('Set-Cookie', `${NAME}=${group}; path=/`)
+    return response
   }
-  addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-  })
+}
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
